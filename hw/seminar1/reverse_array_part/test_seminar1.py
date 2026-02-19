a3cdc870af10111ca0f50c30d7c3268af0a0ee04
Руслан <rusmipt@gmail.com>
2026-02-19 23:04:44 +0300

    add test explanation

--- hw/seminar1/reverse_array_part/test_seminar1.py
+++ hw/seminar1/reverse_array_part/test_seminar1.py
@@ -8,7 +8,7 @@ def test_reverse_array():
     reverse_array(temp)
     assert temp == [4, 3, 2, 1, 0]
 
-    # center
+    # inside part
     reverse_array(temp, start=1, end=4)
     assert temp == [4, 1, 2, 3, 0]
 
@@ -18,6 +18,7 @@ def test_reverse_array_part():
     reverse_array_part(default, 3)
     assert default == [5, 6, 7, 1, 2, 3, 4]
 
+    # multiple cycles
     temp = [i for i in range(5)]  # [0, 1, 2, 3, 4]
     reverse_array_part(temp, 5 + 3)
     assert temp == [2, 3, 4, 0, 1]
