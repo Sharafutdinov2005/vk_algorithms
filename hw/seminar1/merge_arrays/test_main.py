9e8ac619e9eca138671bfc6d951ad2b6922f392b
Руслан <rusmipt@gmail.com>
2026-02-19 23:29:12 +0300

    add more non-balanced tests

--- hw/seminar1/merge_arrays/test_main.py
+++ hw/seminar1/merge_arrays/test_main.py
@@ -11,3 +11,5 @@ def test_merge_edge_cases():
 def test_merge():
     assert merge_arrays([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
     assert merge_arrays([1, 1, 5], [2, 4, 6]) == [1, 1, 2, 4, 5, 6]
+    assert merge_arrays([3], [2, 4, 6]) == [2, 3, 4, 6]
+    assert merge_arrays([1, 5, 6], [2]) == [1, 2, 5, 6]
